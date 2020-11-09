21240216ba8e159c309bb0c161c1e4f481241639
vikramraisharma <vikram.sharma1024@gmail.com>
2020-11-09 15:18:05 -0500

    Update firstrun.py
    Remove essential information, add comments to describe processes

--- firstrun.py
+++ firstrun.py
@@ -4,9 +4,9 @@ def main():
 
     mydb = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="superuser",
-        database="work"
+        user="username",
+        password="password",
+        database="database"
     )
 
     mycursor = mydb.cursor()
@@ -14,21 +14,27 @@ def main():
     mycursor.execute("SELECT inquiry_id, path FROM dsims_attach;")
 
     myresult = mycursor.fetchall()
+    
+    # Saves inquiry result in tuple named myresult
 
     # print(myresult)
     
     add_abstract = ("UPDATE dsims_attach "
         "SET abstract = %(abstract)s "
         "WHERE inquiry_id = %(inquiry_id)s;")
+        # Creates SQL query with embedded variables
     for i in myresult:
         kind = filetype.guess(i[1])
         if kind.extension == "pdf":
+            # Use filetype library to determine if file is PDF
             string = (i[1] + " is a pdf!")
             data_pdf = {
                 'inquiry_id': i[0],
                 'abstract': string,
             }
+            # Store variables set in earlier query
             mycursor.execute(add_abstract, data_pdf)
+            # Execute query with added string and ID
             mydb.commit()
         else:
             # currently no docx support in filetype python lib
