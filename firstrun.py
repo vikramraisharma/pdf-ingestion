782893ebc8a8d2b6225599d37344e46400349e9f
Vikram Sharma <vikram.sharma1024@gmail.com>
2020-11-10 17:00:52 -0500

    add sql config file

--- firstrun.py
+++ firstrun.py
@@ -5,10 +5,7 @@ import pdftotext
 def main():
 
     mydb = mysql.connector.connect(
-        host="localhost",
-        user="username",
-        password="password",
-        database="database"
+        option_files="mysql_config.cnf"
     )
 
     mycursor = mydb.cursor()
@@ -16,38 +13,39 @@ def main():
     # save set of path variables and corresponding IDs in tuple
     mycursor.execute("SELECT inquiry_id, path FROM dsims_attach;")
     myresult = mycursor.fetchall()
-    # print(myresult)
-        missingPDF = open("missing_files.txt", "w")    
+    print(myresult)
+    # missingPDF = open("missing_files.txt", "w")    
 
     
-    add_abstract = ("UPDATE dsims_attach "
-        "SET abstract = %(abstract)s "
-        "WHERE inquiry_id = %(inquiry_id)s;")
-        # Creates SQL query with embedded variables
+    # add_abstract = ("UPDATE dsims_attach "
+    #     "SET abstract = %(abstract)s "
+    #     "WHERE inquiry_id = %(inquiry_id)s;")
+    #     # Creates SQL query with embedded variables
 
-    for i in myresult:
-        # Use filetype library to determine if file is PDF
-        kind = filetype.guess(i[1])
-        if kind.extension == "pdf":
-            # convert the file to txt if it is PDF 
-            with open(i[1], "rb") as f:
-                # pdf = pdftotext.PDF(f, "secret")
+    # for i in myresult:
+    #     # Use filetype library to determine if file is PDF
+    #     kind = filetype.guess(i[1])
+    #     if kind.extension == "pdf":
+    #         # convert the file to txt if it is PDF 
+    #         with open(i[1], "rb") as f:
+    #             # pdf = pdftotext.PDF(f, "secret")
             
-            # define string as abstract from text file
+    #         # define string as abstract from text file
             
-            string = (i[1] + " is a pdf!")
-            data_pdf = {
-                'inquiry_id': i[0],
-                'abstract': string,
-            }
-            # Store variables set in earlier query
-            mycursor.execute(add_abstract, data_pdf)
-            # Execute query with added string and ID
-            mydb.commit()
-        else:
-            missingPDF.write(i[1] + " is not a PDF.")
-            missingPDF.write("\n")
-    missingPDF.close() # close the file to keep track of files that are not PDF or are not writable
+    #         string = (i[1] + " is a pdf!")
+
+    #         data_pdf = {
+    #             'inquiry_id': i[0],
+    #             'abstract': string,
+    #         }
+    #         # Store variables set in earlier query
+    #         mycursor.execute(add_abstract, data_pdf)
+    #         # Execute query with added string and ID
+    #         mydb.commit()
+    #     else:
+    #         missingPDF.write(i[1] + " is not a PDF.")
+    #         missingPDF.write("\n")
+    # missingPDF.close() # close the file to keep track of files that are not PDF or are not writable
 
 if __name__ == "__main__":
     main()
